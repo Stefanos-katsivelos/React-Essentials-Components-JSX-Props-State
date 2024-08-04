9aa8d35af69537fb8c3e0132868468955fdd80a1
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-04 19:31:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+            image={CORE_CONCEPTS[0].image}
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton label="Components"/>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
